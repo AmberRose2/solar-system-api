59395c1e9759149d8d4b5417d2502c64f5ee6580
Alexa Coffman <coffman.alexa@gmail.com>
2022-04-22 12:00:45 -0700

    Fix create_app

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,6 @@ def create_app(test_config=None):
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> aaded4f914e3e01131f9ae5cf6ff2b26ce272db8
     return app
 
 
